76870c949daa742914c73f62d23800387053afd5
Dominic Ojeda <dominicojeda.coding@gmail.com>
2019-02-19 17:18:15 -0700

    duplicate letters no longer work

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,7 +10,7 @@ document.getElementById("guessCount").innerHTML = guessesLeft;
 
 var wordList = ["fish", "cat", "zebra", "gorilla"];
 
-
+var allGuesses = [];
 
 
 console.log(wordList);
@@ -23,7 +23,11 @@ var compWord = wordList[Math.floor(Math.random() * wordList.length)];
 console.log(compWord);
 
 function getLetters() {
+
     var wordLetters = [];
+
+    document.getElementById("currentWord").innerHTML = "";
+
     for (var i = 0; i < compWord.length; i++) {
 
         var arrayLetter = compWord.charAt(i);
@@ -58,18 +62,61 @@ function checkKeyPress(key) {
 
     var keyPress = event.key.toLowerCase();
 
+
+
     if (key.keyCode >= "65" && key.keyCode <= "90") {
         console.log("Player Guess: " + keyPress);
 
-        for (var i = 0; i < compWord.length; i++) {
+        if (allGuesses.indexOf(keyPress) === -1) {
 
-            if (keyPress === compWord.charAt(i)) {
-                document.getElementById("letter" + i).innerHTML = compWord.charAt(i);
-                console.log("the same");
-            } else {
-                console.log("nope")
+            allGuesses.push(keyPress);
+            console.log(allGuesses);
+
+            var correctLetter = false;
+
+
+
+            for (var i = 0; i < compWord.length; i++) {
+
+                if (keyPress === compWord.charAt(i)) {
+
+
+
+                    document.getElementById("letter" + i).innerHTML = compWord.charAt(i);
+                    correctLetter = true;
+                    console.log(correctLetter);
+                } else {
+                    console.log("nope")
+                }
             }
-        }
 
+            if (correctLetter === false) {
+
+                if (guessesLeft > 1) {
+                    guessesLeft--;
+                    console.log(guessesLeft);
+                    document.getElementById("guessCount").innerHTML = guessesLeft;
+                    document.getElementById("guessList").innerHTML = document.getElementById("guessList").innerHTML + keyPress + " ";
+                } else {
+                    console.log("you lose")
+                    guessesLeft = 10;
+                    losses++;
+                    allGuesses = [];
+                    document.getElementById("lossCount").innerHTML = losses;
+                    document.getElementById("guessCount").innerHTML = guessesLeft;
+                    document.getElementById("guessList").innerHTML = "";
+
+                    document.getElementById("previousLetter").innerHTML = "Wrong! It was <span class='text-danger font-weight-bold text-uppercase'>" + compWord + "</span>.";
+
+                    compWord = wordList[Math.floor(Math.random() * wordList.length)];
+                    getLetters();
+                    console.log(compWord);
+                }
+            }
+        } else {
+            console.log("duplicate letter");
+        }
+    } else {
+        console.log("invalid key");
     }
 }
