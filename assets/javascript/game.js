5503ed7de9af341442c521d0c316d2f0c998d232
Dominic Ojeda <dominicojeda.coding@gmail.com>
2019-02-21 11:36:30 -0700

    added Harry Potter words

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,8 +9,10 @@ document.getElementById("winCount").innerHTML = wins;
 document.getElementById("lossCount").innerHTML = losses;
 document.getElementById("guessCount").innerHTML = guessesLeft;
 
-// var wordList = ["harry potter", "hermione granger", "ron weasley", "jk rowling", "hagrid", "luna lovegood", "neville longbottom", "draco malfoy", "serverus snape", "quaffle", "bludger", "golden snitch", "butterbeer", "hogwarts", "horcrux", "quidditch", "muggle", "dumbledore", "gryffindor", "hufflepuff", "ravenclaw", "slytherin", "voldemort", "patronus", "he who must not be named", "tom riddle", "wand", "magic", "spell", "broomstick", "polyjuice potion", "felix felicis", "sorting hat", "expecto patronum", "windgardium leviosa", "expelliarmus", "avada kedavra", "lumos maxima", "dobby", "owl", "hedwig", "riddikulus", "diagon alley", "mischief managed", "ministry of magic", "azkaban", "honeydukes", "knockturn alley", "hogsmeade", "gringotts bank", "shrieking shack", "the leaky cauldron", "forbidden forest", "wizard", "witch", "goblin", "house elf", "animagus", "werewolf", "dragon", "unicorn", "troll", "restricted section", "invisibility cloak", "order of the phoenix", "spider", "basilisk", "dementor", "liquid death", "deathly hallows", "sorcerors stone", "halfblood price", "chamber of secrets", "goblet of fire", "centaur", "giant", "ghost", "book", "dungeon", "gillyweed", "wormwood", "devils snare", "mermaid", "crystal ball", "the boy who lived", "merlins beard"];
-var wordList = ["harry potter", "devil's snare", "wand"]
+var wordList = ["harry potter", "hermione granger", "ron weasley", "jk rowling", "hagrid", "luna lovegood", "neville longbottom", "draco malfoy", "serverus snape", "quaffle", "bludger", "golden snitch", "butterbeer", "hogwarts", "horcrux", "quidditch", "muggle", "dumbledore", "gryffindor", "hufflepuff", "ravenclaw", "slytherin", "voldemort", "patronus", "he who must not be named", "tom marvolo riddle", "wand", "magic", "spell", "broomstick", "polyjuice potion", "felix felicis", "sorting hat", "expecto patronum", "windgardium leviosa", "expelliarmus", "avada kedavra", "lumos maxima", "dobby", "owl", "hedwig", "riddikulus", "diagon alley", "mischief managed", "ministry of magic", "azkaban", "honeydukes", "knockturn alley", "hogsmeade", "gringotts bank", "shrieking shack", "the leaky cauldron", "forbidden forest", "wizard", "witch", "goblin", "house elf", "animagus", "werewolf", "dragon", "unicorn", "troll", "restricted section", "invisibility cloak", "order of the phoenix", "spider", "basilisk", "dementor", "liquid death", "deathly hallows", "sorceror's stone", "halfblood prince", "chamber of secrets", "goblet of fire", "centaur", "giant", "ghost", "book", "dungeon", "gillyweed", "wormwood", "devil's snare", "mermaid", "crystal ball", "the boy who lived", "merlin's beard", "whomping willow", "remus lupin", "scabbers", "hippogriff", "mudblood", "elder wand", "resurrection stone", "imperio", "crucio", "death eater", "auror", "dumbledore's army", "howler", "deluminator", "enchantment", "marauder's map", "wizard's chess", "nagini", "sword", "philosopher's stone", "knight bus", "portkey", "vanishing cabinet", "boggart", "sphinx", "dragon heartstring", "fluffy"];
+
+// test array
+// var wordList = ["harry potter", "devil's snare", "wand"]
 
 var allGuesses = [];
 
@@ -52,7 +54,6 @@ function resetWord() {
             } else {
                 document.getElementById("currentWord").innerHTML = document.getElementById("currentWord").innerHTML + "<span id='letter" + i + "'> &#39; </span>";
                 correctLetterCount++;
-                console.log("WOOOOOOOOOOO");
             }
 
         }
